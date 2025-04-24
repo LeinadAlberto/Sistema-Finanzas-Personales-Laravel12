a1cfbd17f814d5ed378277034e27a05b0e6b8ba1
Alberto <dany.canaviri2022@gmail.com>
2025-04-23 22:29:35 -0400

    Se definio las migraciones y las relaciones del proyecto

---
+++ app/Models/Presupuesto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Presupuesto extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'categoria_id',
+        'monto_asignado',
+        'monto_gastado',
+        'mes',
+        'anio'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+}
